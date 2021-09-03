937f32305285dd8b9784d546310bc64788ce680e
Bernard2030 <bernard.mukhenye@student.moringaschool.com>
2021-09-03 18:45:08 +0300

    add class credential

---
+++ credentials.py
@@ -0,0 +1,11 @@
+class Credentials:
+    """
+    A class that generates new user credentials
+    """
+    pass
+credentials_list = []
+
+def __init__(self, user_name, passward, email):
+    self.user_name = user_name
+    self.passward = passward
+    self.email = email
