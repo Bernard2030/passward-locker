5ae61c5c13afb7812e7e057e0f943ef35cc4d51b
Bernard2030 <bernard.mukhenye@student.moringaschool.com>
2021-09-03 18:37:48 +0300

    add classmethod and delete function

--- passward.py
+++ passward.py
@@ -8,17 +8,32 @@ class User:
     """
     user_list = []
 
-def __init__(self, user_name, passward):
+def __init__(self, user_name, passward, email):
     """
     Method defining user properties
     """
 
     self.user_name = user_name
-    self.passward = passward    
+    self.passward = passward
+    self.email = email    
 
 def save_user(self):
     """
     A method which saves the new user instance on the user_list
     """
 
-    User.user_list.append(self)    
+    User.user_list.append(self) 
+
+@classmethod
+def display_user(cls):
+    """
+    Method thad displays class user
+    """
+    return cls.user_list
+
+def delete_user(self):
+    """
+    delete method deletes a saved passward in our list_user
+    """
+
+    User.user_list.remove(self)
