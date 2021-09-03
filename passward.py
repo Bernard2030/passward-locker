c8b2919768f2f18a5467511ff7888793b8d80a68
Bernard2030 <bernard.mukhenye@student.moringaschool.com>
2021-09-03 17:28:21 +0300

    initialise user method

--- passward.py
+++ passward.py
@@ -7,3 +7,11 @@ class User:
     Class generates new instances of User
     """
     user_list = []
+
+def __init__(self, user_name, passward):
+    """
+    Method defining user properties
+    """
+
+    self.user_name = user_name
+    self.passward = passward    
